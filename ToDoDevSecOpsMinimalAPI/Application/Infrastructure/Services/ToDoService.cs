9560ad3c8467eaeaa6055a21d20c0f7c8cd6224c
Christoffer Jörgensen <71841695+christojor@users.noreply.github.com>
2025-12-17 02:15:38 +0100

    Inout Validation (#7)

--- ToDoDevSecOpsMinimalAPI/Application/Infrastructure/Services/ToDoService.cs
+++ ToDoDevSecOpsMinimalAPI/Application/Infrastructure/Services/ToDoService.cs
@@ -1,62 +1,52 @@
 namespace ToDoDevSecOpsMinimalAPI.Application.Infrastructure.Services;
 
-using ToDoDevSecOpsMinimalAPI.Application.Common.Models;
-using ToDoDevSecOpsMinimalAPI.Application.Domain.ToDos;
-using ToDoDevSecOpsMinimalAPI.Application.Infrastructure.Persistence;
-
-public class ToDoService : IToDoService
+public class ToDoService(ToDoDbContext context) : IToDoService
 {
-    private readonly ToDoDbContext _db;
-
-    public ToDoService(ToDoDbContext db)
-    {
-        _db = db;
-    }
+    private readonly ToDoDbContext _context = context;
 
     public async Task<TodoReadDTO[]> GetAllAsync()
     {
-        return await _db.Todos.Select(t => new TodoReadDTO(t)).ToArrayAsync();
+        return await _context.Todos.Select(_ => new TodoReadDTO(_)).ToArrayAsync();
     }
 
     public async Task<TodoReadDTO?> GetByIdAsync(int id)
     {
-        var todo = await _db.Todos.FindAsync(id);
+        var todo = await _context.Todos.FindAsync(id);
+
         return todo is null ? null : new TodoReadDTO(todo);
     }
 
     public async Task<TodoReadDTO> CreateAsync(TodoCreateDTO dto)
     {
-        if (dto is null) throw new NullReferenceException();
-        if (string.IsNullOrWhiteSpace(dto.Name)) throw new ArgumentException("Name is required", nameof(dto.Name));
-        if (dto.Name.Length > 100) throw new ArgumentException("Name exceeds maximum length of 100 characters", nameof(dto.Name));
+        var todo = new Todo { Name = dto.Name, IsComplete = dto.IsComplete };
 
-        var name = dto.Name; // local non-null copy
-        var todo = new Todo { Name = name, IsComplete = dto.IsComplete };
-        _db.Todos.Add(todo);
-        await _db.SaveChangesAsync();
+        _context.Todos.Add(todo);
+        await _context.SaveChangesAsync();
+
         return new TodoReadDTO(todo);
     }
 
     public async Task<bool> UpdateAsync(int id, TodoUpdateDTO dto)
     {
-        if (dto is null) throw new NullReferenceException();
-        if (string.IsNullOrWhiteSpace(dto.Name)) throw new ArgumentException("Name is required", nameof(dto.Name));
-        if (dto.Name.Length > 100) throw new ArgumentException("Name exceeds maximum length of 100 characters", nameof(dto.Name));
-
-        var todo = await _db.Todos.FindAsync(id);
+        var todo = await _context.Todos.FindAsync(id);
         if (todo is null) return false;
+
         todo.Name = dto.Name;
         todo.IsComplete = dto.IsComplete;
-        await _db.SaveChangesAsync();
+
+        await _context.SaveChangesAsync();
+
         return true;
     }
 
     public async Task<bool> DeleteAsync(int id)
     {
-        var todo = await _db.Todos.FindAsync(id);
+        var todo = await _context.Todos.FindAsync(id);
         if (todo is null) return false;
-        _db.Todos.Remove(todo);
-        await _db.SaveChangesAsync();
+
+        _context.Todos.Remove(todo);
+        await _context.SaveChangesAsync();
+
         return true;
     }
 }
---
+++ ToDoDevSecOpsMinimalAPI/Application/Validators/ToDoValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoDevSecOpsMinimalAPI.Application.Validators;
+
+public class CreateToDoValidator : AbstractValidator<TodoCreateDTO>
+{
+    public CreateToDoValidator()
+    {
+        RuleFor(_ => _.Name)
+            .NotNull()
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
+
+public class UpdateToDoValidator : AbstractValidator<TodoUpdateDTO>
+{
+    public UpdateToDoValidator()
+    {
+        RuleFor(_ => _.Name)
+            .NotNull()
+            .NotEmpty()
+            .MaximumLength(100);
+    }
+}
